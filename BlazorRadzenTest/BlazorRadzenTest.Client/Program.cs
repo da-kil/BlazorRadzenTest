f1f75a8da4bc9afcf00d3ede2791e76c2bcd5108
Daniel Killen <daniel.killen@ti8m.ch>
2025-09-08 13:11:06 +0200

    init check-in

---
+++ BlazorRadzenTest/BlazorRadzenTest.Client/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+
+namespace BlazorRadzenTest.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddRadzenComponents();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
