117c721090853ac60c93b6f4d499bcc3c27b192d
Daniel Killen <daniel.killen@ti8m.ch>
2025-09-15 16:26:13 +0200

    add ordering of items

--- 05_Frontend/ti8m.BeachBreak.Client/Models/CompetencyDefinition.cs
+++ 05_Frontend/ti8m.BeachBreak.Client/Models/CompetencyDefinition.cs
@@ -2,16 +2,18 @@ namespace ti8m.BeachBreak.Client.Models;
 
 public class CompetencyDefinition
 {
-    public CompetencyDefinition(string key, string title, string description, bool isRequired)
+    public CompetencyDefinition(string key, string title, string description, bool isRequired, int order = 0)
     {
         Key = key;
         Title = title;
         Description = description;
         IsRequired = isRequired;
+        Order = order;
     }
 
     public string Key { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public bool IsRequired { get; set; }
+    public int Order { get; set; }
 }
