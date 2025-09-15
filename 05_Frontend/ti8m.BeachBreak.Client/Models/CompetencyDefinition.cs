461b7ba5f64b2c93a713ae8f936b10c86fbb4791
Daniel Killen <daniel.killen@ti8m.ch>
2025-09-15 13:56:54 +0200

    inline editing

--- 05_Frontend/ti8m.BeachBreak.Client/Models/CompetencyDefinition.cs
+++ 05_Frontend/ti8m.BeachBreak.Client/Models/CompetencyDefinition.cs
@@ -2,14 +2,16 @@ namespace ti8m.BeachBreak.Client.Models;
 
 public class CompetencyDefinition
 {
-    public CompetencyDefinition(string key, string title, string description)
+    public CompetencyDefinition(string key, string title, string description, bool isRequired)
     {
         Key = key;
         Title = title;
         Description = description;
+        IsRequired = isRequired;
     }
 
     public string Key { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public bool IsRequired { get; set; }
 }
