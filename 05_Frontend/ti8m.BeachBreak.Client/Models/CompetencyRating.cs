e1c2f6406d38c1bf4830a31f4d7b088cf1baa10d
Killen Daniel <dani@killen.ch>
2025-09-25 06:59:42 +0200

    cleanup

--- 05_Frontend/ti8m.BeachBreak.Client/Models/CompetencyRating.cs
+++
@@ -1,7 +0,0 @@
-namespace ti8m.BeachBreak.Client.Models;
-
-public class CompetencyRating
-{
-    public int Rating { get; set; }
-    public string Comment { get; set; } = string.Empty;
-}
--- 05_Frontend/ti8m.BeachBreak.Client/Models/GoalAchievement.cs
+++
@@ -1,9 +0,0 @@
-namespace ti8m.BeachBreak.Client.Models;
-
-public class GoalAchievement
-{
-    public string Description { get; set; } = string.Empty;
-    public int AchievementPercentage { get; set; }
-    public string EmployeeJustification { get; set; } = string.Empty;
-    public string SupervisorJustification { get; set; } = string.Empty;
-}
