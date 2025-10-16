0576a04d9a8979ce74e5c203783988ecfe1fbf29
Dani <dani@killen.ch>
2025-10-16 21:19:28 +0200

    clean up

---
+++ CleanDatabaseTables.sql
@@ -0,0 +1,10 @@
+DELETE FROM events.mt_doc_deadletterevent;
+DELETE FROM events.mt_event_progression;
+DELETE FROM events.mt_events;
+DELETE FROM events.mt_streams;
+DELETE FROM readmodels.mt_doc_categoryreadmodel;
+DELETE FROM readmodels.mt_doc_employeereadmodel;
+DELETE FROM readmodels.mt_doc_organizationreadmodel;
+DELETE FROM readmodels.mt_doc_questionnaireassignmentreadmodel;
+DELETE FROM readmodels.mt_doc_questionnaireresponsereadmodel;
+DELETE FROM readmodels.mt_doc_questionnairetemplatereadmodel;
