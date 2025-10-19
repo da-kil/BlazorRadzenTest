50a12cf223bb8c5cc48adfd3d7e105021d80423d
Dani <dani@killen.ch>
2025-10-19 20:46:40 +0200

    fix cookie refresh error

--- 05_Frontend/ti8m.BeachBreak/CookieOidcRefresher.cs
+++ 05_Frontend/ti8m.BeachBreak/CookieOidcRefresher.cs
@@ -36,67 +36,83 @@ internal sealed class CookieOidcRefresher(IOptionsMonitor<OpenIdConnectOptions> 
             return;
         }
 
-        var oidcConfiguration = await oidcOptions.ConfigurationManager!.GetConfigurationAsync(validateContext.HttpContext.RequestAborted);
-        var tokenEndpoint = oidcConfiguration.TokenEndpoint ?? throw new InvalidOperationException("Cannot refresh cookie. TokenEndpoint missing!");
+        try
+        {
+            var oidcConfiguration = await oidcOptions.ConfigurationManager!.GetConfigurationAsync(validateContext.HttpContext.RequestAborted);
+            var tokenEndpoint = oidcConfiguration.TokenEndpoint ?? throw new InvalidOperationException("Cannot refresh cookie. TokenEndpoint missing!");
 
-        using var refreshResponse = await oidcOptions.Backchannel.PostAsync(tokenEndpoint,
-            new FormUrlEncodedContent(new Dictionary<string, string?>()
+            using var refreshResponse = await oidcOptions.Backchannel.PostAsync(tokenEndpoint,
+                new FormUrlEncodedContent(new Dictionary<string, string?>()
+                {
+                    ["grant_type"] = "refresh_token",
+                    ["client_id"] = oidcOptions.ClientId,
+                    ["client_secret"] = oidcOptions.ClientSecret,
+                    ["scope"] = string.Join(" ", oidcOptions.Scope),
+                    ["refresh_token"] = validateContext.Properties.GetTokenValue("refresh_token"),
+                }));
+
+            if (!refreshResponse.IsSuccessStatusCode)
             {
-                ["grant_type"] = "refresh_token",
-                ["client_id"] = oidcOptions.ClientId,
-                ["client_secret"] = oidcOptions.ClientSecret,
-                ["scope"] = string.Join(" ", oidcOptions.Scope),
-                ["refresh_token"] = validateContext.Properties.GetTokenValue("refresh_token"),
-            }));
+                validateContext.RejectPrincipal();
+                return;
+            }
 
-        if (!refreshResponse.IsSuccessStatusCode)
+            var refreshJson = await refreshResponse.Content.ReadAsStringAsync();
+            var message = new OpenIdConnectMessage(refreshJson);
+
+            var validationParameters = oidcOptions.TokenValidationParameters.Clone();
+            if (oidcOptions.ConfigurationManager is BaseConfigurationManager baseConfigurationManager)
+            {
+                validationParameters.ConfigurationManager = baseConfigurationManager;
+            }
+            else
+            {
+                validationParameters.ValidIssuer = oidcConfiguration.Issuer;
+                validationParameters.IssuerSigningKeys = oidcConfiguration.SigningKeys;
+            }
+
+            var validationResult = await oidcOptions.TokenHandler.ValidateTokenAsync(message.IdToken, validationParameters);
+
+            if (!validationResult.IsValid)
+            {
+                validateContext.RejectPrincipal();
+                return;
+            }
+
+            var validatedIdToken = JwtSecurityTokenConverter.Convert(validationResult.SecurityToken as JsonWebToken);
+            validatedIdToken.Payload["nonce"] = null;
+            oidcTokenValidator.ValidateTokenResponse(new()
+            {
+                ProtocolMessage = message,
+                ClientId = oidcOptions.ClientId,
+                ValidatedIdToken = validatedIdToken,
+            });
+
+            validateContext.ShouldRenew = true;
+            validateContext.ReplacePrincipal(new ClaimsPrincipal(validationResult.ClaimsIdentity));
+
+            var expiresIn = int.Parse(message.ExpiresIn, NumberStyles.Integer, CultureInfo.InvariantCulture);
+            var expiresAt = now + TimeSpan.FromSeconds(expiresIn);
+            validateContext.Properties.StoreTokens([
+                new() { Name = "access_token", Value = message.AccessToken },
+                new() { Name = "id_token", Value = message.IdToken },
+                new() { Name = "refresh_token", Value = message.RefreshToken },
+                new() { Name = "token_type", Value = message.TokenType },
+                new() { Name = "expires_at", Value = expiresAt.ToString("o", CultureInfo.InvariantCulture) },
+            ]);
+        }
+        catch (HttpRequestException)
         {
+            // Network error (DNS failure, connection timeout, etc.) - reject principal to force re-authentication
+            // This will redirect the user to login page
             validateContext.RejectPrincipal();
             return;
         }
-
-        var refreshJson = await refreshResponse.Content.ReadAsStringAsync();
-        var message = new OpenIdConnectMessage(refreshJson);
-
-        var validationParameters = oidcOptions.TokenValidationParameters.Clone();
-        if (oidcOptions.ConfigurationManager is BaseConfigurationManager baseConfigurationManager)
+        catch (TaskCanceledException)
         {
-            validationParameters.ConfigurationManager = baseConfigurationManager;
-        }
-        else
-        {
-            validationParameters.ValidIssuer = oidcConfiguration.Issuer;
-            validationParameters.IssuerSigningKeys = oidcConfiguration.SigningKeys;
-        }
-
-        var validationResult = await oidcOptions.TokenHandler.ValidateTokenAsync(message.IdToken, validationParameters);
-
-        if (!validationResult.IsValid)
-        {
+            // Request timeout - reject principal to force re-authentication
             validateContext.RejectPrincipal();
             return;
         }
-
-        var validatedIdToken = JwtSecurityTokenConverter.Convert(validationResult.SecurityToken as JsonWebToken);
-        validatedIdToken.Payload["nonce"] = null;
-        oidcTokenValidator.ValidateTokenResponse(new()
-        {
-            ProtocolMessage = message,
-            ClientId = oidcOptions.ClientId,
-            ValidatedIdToken = validatedIdToken,
-        });
-
-        validateContext.ShouldRenew = true;
-        validateContext.ReplacePrincipal(new ClaimsPrincipal(validationResult.ClaimsIdentity));
-
-        var expiresIn = int.Parse(message.ExpiresIn, NumberStyles.Integer, CultureInfo.InvariantCulture);
-        var expiresAt = now + TimeSpan.FromSeconds(expiresIn);
-        validateContext.Properties.StoreTokens([
-            new() { Name = "access_token", Value = message.AccessToken },
-            new() { Name = "id_token", Value = message.IdToken },
-            new() { Name = "refresh_token", Value = message.RefreshToken },
-            new() { Name = "token_type", Value = message.TokenType },
-            new() { Name = "expires_at", Value = expiresAt.ToString("o", CultureInfo.InvariantCulture) },
-        ]);
     }
 }
