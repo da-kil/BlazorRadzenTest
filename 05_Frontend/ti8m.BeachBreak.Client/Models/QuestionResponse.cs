b27095106727706feef89aefa7c4b90f5d1bfac5
Dani <dani@killen.ch>
2025-10-22 22:33:34 +0200

    couple of fixes

--- 05_Frontend/ti8m.BeachBreak.Client/Models/QuestionResponse.cs
+++ 05_Frontend/ti8m.BeachBreak.Client/Models/QuestionResponse.cs
@@ -6,4 +6,9 @@ public class QuestionResponse
     public QuestionType QuestionType { get; set; }
     public Dictionary<string, object>? ComplexValue { get; set; } // All answer data stored here
     public DateTime LastModified { get; set; } = DateTime.Now;
+
+    // Track edits made during review meeting
+    public bool EditedDuringReview { get; set; }
+    public Guid? EditedDuringReviewBy { get; set; }
+    public DateTime? EditedDuringReviewAt { get; set; }
 }
